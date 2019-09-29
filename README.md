31657864ee871127037d93fe40a94565d0306135
Jonas Riedel <riedel@klaxx.net>
2019-09-29 17:01:05 +0200

    change README

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # Object detection with imageAI
 
-Install [imageAI](http://imageai.org/)
+Based on [imageAI](http://imageai.org/)
+
+Import conda environment with
+
+```conda env create -f environment.yml```
 
 Download [YoloV3 Model](https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5)
 
 
+
 ## imageobjectdetection.py
 Uses all ```*.jpg``` Files from the ```pictures``` directory and performes object detection on them. Saves the result in ```*_new.jpg``` in same folder
+
+## videoobjectdetection.py
+Uses Webcam to capture frames an then performs object detection. Shows result in a window.
--- imageobjectdetection.py
+++ imageobjectdetection.py
@@ -10,11 +10,15 @@ detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
 detector.loadModel()
 
 
-files = glob.glob("pictures/*.jpg")
+files = glob.glob("pictures/*[0-9].jpg")
 
 for f in files:
+    newFile=os.path.join(execution_path , f.replace(".jpg", "_new.jpg"))
     print("File: ", f)
-    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , f), output_image_path=os.path.join(execution_path , f.replace(".jpg", "_new.jpg")), minimum_percentage_probability=30)
+    
+    if os.path.isfile(newFile)==True:
+        continue
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , f), output_image_path=newFile, minimum_percentage_probability=30)
 
     for eachObject in detections:
         print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
